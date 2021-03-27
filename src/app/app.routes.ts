eeb64bc293ba6dad97f0cf96701111ac6761ed8e
@ewilliaml <willian_enrriquez89@hotmail.com>
2021-03-27 09:17:59 -0500

    Mostrando mensajes de errores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarouselComponent } from './pages/carousel/carousel.component';
 import { CarouselSponsorComponent } from './pages/carousel-sponsor/carousel-sponsor.component';
 import { AuthenticationComponent } from './pages/authentication/authentication.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'carousel', component: CarouselComponent },
   { path: 'carousel-sponsor', component: CarouselSponsorComponent },
   { path: 'login', component: AuthenticationComponent },
+  { path: 'dashboard', component: DashboardComponent },
+
   { path: '**', pathMatch: 'full', redirectTo: `login` },
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { CarouselSponsorComponent } from './carousel-sponsor/carousel-sponsor.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { SwiperModule } from 'ngx-swiper-wrapper';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PrimengModule } from '../modulos/primeng.module';
+
+const pagesModules = [CommonModule, SwiperModule, PrimengModule];
 
 @NgModule({
-  declarations: [CarouselComponent, CarouselSponsorComponent],
-  imports: [CommonModule, SwiperModule],
-  exports: [CarouselComponent, CarouselSponsorComponent],
+  declarations: [
+    CarouselComponent,
+    CarouselSponsorComponent,
+    DashboardComponent,
+  ],
+  imports: [pagesModules],
+  exports: [pagesModules],
 })
 export class PagesModule {}
